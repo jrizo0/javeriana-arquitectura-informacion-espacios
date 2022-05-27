4fb24d404db853e69736d1d4b0eda99ff46106b6
Julian Rizo <jr.rizo.o.jr@gmail.com>
2022-05-26 20:42:59 -0500

    cambios

--- src/main/java/co/edu/javeriana/as/configuration/LoadData.java
+++ src/main/java/co/edu/javeriana/as/configuration/LoadData.java
@@ -14,18 +14,18 @@ class LoadData {
     @Bean
     CommandLineRunner initDatabase(EdificiosRepository edificiosRepository, EspaciosRepository espaciosRepository){
         return args -> {
-            System.out.println("Cargando información...");
-            Edificio baron = new Edificio(3, "Barón");
-            Edificio cubos = new Edificio(45, "Cubos");
-            Espacio salon30 = new Espacio("salon30", 40, baron);
-            Espacio salon200 = new Espacio("salon200", 30, cubos);
-            Espacio salon201 = new Espacio("salon201", 30, cubos);
-            edificiosRepository.save(baron);
-            edificiosRepository.save(cubos);
-            espaciosRepository.save(salon30);
-            espaciosRepository.save(salon200);
-            espaciosRepository.save(salon201);
-            System.out.println("Termina el cargue de información.");
+//            System.out.println("Cargando información...");
+//            Edificio baron = new Edificio(3, "Barón");
+//            Edificio cubos = new Edificio(45, "Cubos");
+//            Espacio salon30 = new Espacio("salon30", 40, baron);
+//            Espacio salon200 = new Espacio("salon200", 30, cubos);
+//            Espacio salon201 = new Espacio("salon201", 30, cubos);
+//            edificiosRepository.save(baron);
+//            edificiosRepository.save(cubos);
+//            espaciosRepository.save(salon30);
+//            espaciosRepository.save(salon200);
+//            espaciosRepository.save(salon201);
+//            System.out.println("Termina el cargue de información.");
         };
     }
 }
